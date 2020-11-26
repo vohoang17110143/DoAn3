3a6dc2a80a98de14088590f06590065a3e877f15
NguyenVoHoang <17110143@student.hcmute.edu.vn>
2020-11-26 21:05:02 +0700

    Code-Demo

---
+++ Accord demo/Accord demo/MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Accord_demo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoSourcePlayer1 = new Accord.Controls.VideoSourcePlayer();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonOption = new System.Windows.Forms.Button();
+            this.buttonDetect = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.devicesCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(93, 50);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(942, 423);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new Accord.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackgroundImage = global::Accord_demo.Properties.Resources.shutdown;
+            this.buttonClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonClose.Location = new System.Drawing.Point(1014, 517);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(99, 104);
+            this.buttonClose.TabIndex = 4;
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonOption
+            // 
+            this.buttonOption.BackgroundImage = global::Accord_demo.Properties.Resources.img_239291;
+            this.buttonOption.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonOption.Location = new System.Drawing.Point(854, 517);
+            this.buttonOption.Name = "buttonOption";
+            this.buttonOption.Size = new System.Drawing.Size(99, 104);
+            this.buttonOption.TabIndex = 3;
+            this.buttonOption.UseVisualStyleBackColor = true;
+            this.buttonOption.Click += new System.EventHandler(this.buttonOption_Click);
+            // 
+            // buttonDetect
+            // 
+            this.buttonDetect.BackgroundImage = global::Accord_demo.Properties.Resources.detect;
+            this.buttonDetect.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonDetect.Location = new System.Drawing.Point(213, 517);
+            this.buttonDetect.Name = "buttonDetect";
+            this.buttonDetect.Size = new System.Drawing.Size(99, 104);
+            this.buttonDetect.TabIndex = 2;
+            this.buttonDetect.UseVisualStyleBackColor = true;
+            this.buttonDetect.Click += new System.EventHandler(this.buttonDetect_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.BackgroundImage = global::Accord_demo.Properties.Resources.play;
+            this.buttonPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonPlay.Location = new System.Drawing.Point(64, 517);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(99, 104);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // devicesCombo
+            // 
+            this.devicesCombo.FormattingEnabled = true;
+            this.devicesCombo.Location = new System.Drawing.Point(453, 556);
+            this.devicesCombo.Name = "devicesCombo";
+            this.devicesCombo.Size = new System.Drawing.Size(247, 28);
+            this.devicesCombo.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(449, 517);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Select Device: ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 633);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.devicesCombo);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonOption);
+            this.Controls.Add(this.buttonDetect);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Accord.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonDetect;
+        private System.Windows.Forms.Button buttonOption;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.ComboBox devicesCombo;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Accord demo/Accord demo/MainForm.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Accord.Imaging.Filters;
+using Accord.Vision.Detection;
+using Accord.Vision.Detection.Cascades;
+using Accord.Vision.Tracking;
+using Accord.Imaging;
+using Accord.Math.Geometry;
+using Accord.Video;
+using Accord.Video.DirectShow;
+using Accord.Video.VFW;
+using System.Threading;
+
+namespace Accord_demo
+{
+    public partial class MainForm : Form
+    {
+        private IVideoSource videoSource = null;
+
+        private HaarObjectDetector detector;
+
+        private Camshift tracker = null;
+
+        private RectanglesMarker marker;
+
+        private bool detecting = false;
+        private bool tracking = false;
+
+        private FilterInfoCollection videoDevices;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            HaarCascade cascade = new FaceHaarCascade();
+            detector = new HaarObjectDetector(cascade,
+                25, ObjectDetectorSearchMode.Single, 1.2f,
+                ObjectDetectorScalingMode.GreaterToSmaller);
+
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                foreach (FilterInfo device in videoDevices)
+                {
+                    devicesCombo.Items.Add(device.Name);
+                }
+            }
+            catch (ApplicationException)
+            {
+                devicesCombo.Items.Add("Không tìm thấy thiết bị ghi hình");
+                devicesCombo.Enabled = false;
+                buttonPlay.Enabled = false;
+            }
+
+            devicesCombo.SelectedIndex = 0;
+        }
+
+        private void OpenVideoSource(IVideoSource source)
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            CloseVideoSource();
+
+            videoSourcePlayer1.VideoSource = source;
+            videoSourcePlayer1.Start();
+
+            videoSource = source;
+
+            this.Cursor = Cursors.Default;
+        }
+
+        private void CloseVideoSource()
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            videoSourcePlayer1.SignalToStop();
+
+            for (int i = 0; (i < 50) && (videoSourcePlayer1.IsRunning); i++)
+            {
+                Thread.Sleep(100);
+            }
+            if (videoSourcePlayer1.IsRunning)
+                videoSourcePlayer1.Stop();
+
+            tracker = new Camshift();
+
+            tracker.Conservative = true;
+            tracker.AspectRatio = 1.5f;
+
+            videoSourcePlayer1.BorderColor = Color.Black;
+            this.Cursor = Cursors.Default;
+        }
+
+        private static VideoCapabilities selectResolution(VideoCaptureDevice device)
+        {
+            foreach (var cap in device.VideoCapabilities)
+            {
+                if (cap.FrameSize.Height == 240)
+                    return cap;
+                if (cap.FrameSize.Width == 320)
+                    return cap;
+            }
+
+            return device.VideoCapabilities.Last();
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[devicesCombo.SelectedIndex].MonikerString);
+
+            videoSource.VideoResolution = selectResolution(videoSource);
+
+            OpenVideoSource(videoSource);
+        }
+
+        private void buttonOption_Click(object sender, EventArgs e)
+        {
+            if ((videoSource != null) && (videoSource is VideoCaptureDevice))
+            {
+                try
+                {
+                    ((VideoCaptureDevice)videoSource).DisplayPropertyPage(this.Handle);
+                }
+                catch (NotSupportedException)
+                {
+                    MessageBox.Show("Không có video ", "Lỗi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseVideoSource();
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            if (!detecting && !tracking)
+                return;
+
+            lock (this)
+            {
+                if (detecting)
+                {
+                    detecting = false;
+                    tracking = false;
+
+                    UnmanagedImage im = UnmanagedImage.FromManagedImage(image);
+
+                    float xscale = image.Width / 160f;
+                    float yscale = image.Height / 120f;
+
+                    ResizeNearestNeighbor resize = new ResizeNearestNeighbor(160, 120);
+                    UnmanagedImage downsample = resize.Apply(im);
+
+                    Rectangle[] regions = detector.ProcessFrame(downsample);
+
+                    if (regions.Length > 0)
+                    {
+                        tracker.Reset();
+
+                        Rectangle face = regions[0];
+
+                        Rectangle window = new Rectangle(
+                            (int)((regions[0].X + regions[0].Width / 2f) * xscale),
+                            (int)((regions[0].Y + regions[0].Height / 2f) * yscale),
+                            1, 1);
+
+                        window.Inflate(
+                            (int)(0.2f * regions[0].Width * xscale),
+                            (int)(0.4f * regions[0].Height * yscale));
+
+                        tracker.SearchWindow = window;
+                        tracker.ProcessFrame(im);
+
+                        marker = new RectanglesMarker(window);
+                        marker.ApplyInPlace(im);
+
+                        image = im.ToManagedImage();
+
+                        tracking = true;
+                        detecting = true;
+                    }
+                    else
+                    {
+                        detecting = true;
+                    }
+                }
+                else
+                {
+                    if (marker != null)
+                        image = marker.Apply(image);
+                }
+            }
+        }
+
+        private void buttonDetect_Click(object sender, EventArgs e)
+        {
+            detecting = true;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
