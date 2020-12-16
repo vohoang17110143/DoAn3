9d7bbab6dc84f2b88c3a81e5d691e1b5f24184af
NguyenVoHoang <17110143@student.hcmute.edu.vn>
2020-12-16 21:22:42 +0700

    New update

---
+++ Record_Audio/Record_Audio/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Record_Audio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.wavechart1 = new Accord.Controls.Wavechart();
+            this.lbLength = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonRecord = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lbLength);
+            this.panel1.Controls.Add(this.wavechart1);
+            this.panel1.Location = new System.Drawing.Point(64, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1061, 291);
+            this.panel1.TabIndex = 0;
+            // 
+            // wavechart1
+            // 
+            this.wavechart1.BackColor = System.Drawing.Color.Black;
+            this.wavechart1.ForeColor = System.Drawing.Color.Lime;
+            this.wavechart1.Location = new System.Drawing.Point(90, 52);
+            this.wavechart1.Name = "wavechart1";
+            this.wavechart1.RangeX = ((Accord.DoubleRange)(resources.GetObject("wavechart1.RangeX")));
+            this.wavechart1.RangeY = ((Accord.DoubleRange)(resources.GetObject("wavechart1.RangeY")));
+            this.wavechart1.SimpleMode = false;
+            this.wavechart1.Size = new System.Drawing.Size(764, 129);
+            this.wavechart1.TabIndex = 0;
+            this.wavechart1.Text = "wavechart1";
+            // 
+            // lbLength
+            // 
+            this.lbLength.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbLength.Location = new System.Drawing.Point(888, 52);
+            this.lbLength.Name = "lbLength";
+            this.lbLength.Size = new System.Drawing.Size(117, 133);
+            this.lbLength.TabIndex = 1;
+            this.lbLength.Text = "0.00.00 sec";
+            this.lbLength.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1231, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Wave files (*.wav)|*.wav";
+            this.saveFileDialog1.Title = "Save file";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Wave files (*.wav)|*.wav";
+            this.openFileDialog1.Title = "Open file";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(506, 487);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(128, 73);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Location = new System.Drawing.Point(871, 487);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(128, 73);
+            this.buttonRecord.TabIndex = 3;
+            this.buttonRecord.Text = "Record";
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.buttonRecord_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(206, 487);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(128, 73);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(64, 402);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(1061, 69);
+            this.trackBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 584);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonRecord);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Accord.Controls.Wavechart wavechart1;
+        private System.Windows.Forms.Label lbLength;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonRecord;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Record_Audio/Record_Audio/Form1.cs
@@ -0,0 +1,237 @@
+﻿using Accord.Audio;
+using Accord.Audio.Formats;
+using Accord.DirectSound;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Record_Audio
+{
+    public partial class Form1 : Form
+    {
+        private MemoryStream stream;
+
+        private IAudioSource source;
+        private IAudioOutput output;
+
+        private WaveEncoder encoder;
+        private WaveDecoder decoder;
+
+        private float[] current;
+
+        private int frames;
+        private int sample;
+
+        private TimeSpan duration;
+
+        public Form1()
+        {
+            InitializeComponent();
+            wavechart1.SimpleMode = true;
+            wavechart1.AddWaveform("wave", Color.Green, 1, false);
+            updateButtons();
+        }
+
+        private void updateButtons()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(updateButtons));
+                return;
+            }
+
+            if (source != null && source.IsRunning)
+            {
+                buttonPlay.Enabled = false;
+                buttonStop.Enabled = true;
+                buttonRecord.Enabled = false;
+                trackBar1.Enabled = false;
+            }
+            else if (output != null && output.IsRunning)
+            {
+                buttonPlay.Enabled = false;
+                buttonStop.Enabled = true;
+                buttonRecord.Enabled = false;
+                trackBar1.Enabled = true;
+            }
+            else
+            {
+                buttonPlay.Enabled = stream != null;
+                buttonStop.Enabled = false;
+                buttonRecord.Enabled = true;
+                trackBar1.Enabled = decoder != null;
+                trackBar1.Value = 0;
+            }
+        }
+
+        private void updateWaveForm(float[] sample, int length)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => wavechart1.UpdateWaveform("wave", sample, length)));
+            }
+            else
+            {
+                wavechart1.UpdateWaveform("wave", current, length);
+            }
+        }
+
+        private void updateTrackBar(int value)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => trackBar1.Value = Math.Max(trackBar1.Minimum, Math.Min(trackBar1.Maximum, value))));
+            }
+            else
+            {
+                trackBar1.Value = Math.Max(trackBar1.Minimum, Math.Min(trackBar1.Maximum, value));
+            }
+        }
+
+        private void source_AudioSourceError(object sender, AudioSourceErrorEventArgs e)
+        {
+            throw new Exception(e.Description);
+        }
+
+        private void source_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            eventArgs.Signal.CopyTo(current);
+
+            updateWaveForm(current, eventArgs.Signal.Length);
+            encoder.Encode(eventArgs.Signal);
+            duration += eventArgs.Signal.Duration;
+            sample += eventArgs.Signal.Samples;
+            frames += eventArgs.Signal.Length;
+        }
+
+        private void output__FramePlayingStarted(object sender, PlayFrameEventArgs e)
+        {
+            updateTrackBar(e.FrameIndex);
+            if (e.FrameIndex + e.Count < decoder.Frames)
+            {
+                int previous = decoder.Position;
+                decoder.Seek(e.FrameIndex);
+
+                Signal s = decoder.Decode(e.Count);
+                decoder.Seek(previous);
+                updateWaveForm(s.ToFloat(), s.Length);
+            }
+        }
+
+        private void output_PlayingFinished(object sender, EventArgs e)
+        {
+            updateButtons();
+            Array.Clear(current, 0, current.Length);
+            updateWaveForm(current, current.Length);
+        }
+
+        private void output_NewFrameRequested(object sender, NewFrameRequestedEventArgs e)
+        {
+            e.FrameIndex = decoder.Position;
+
+            Signal signal = decoder.Decode(e.Frames);
+
+            if (signal == null)
+            {
+                e.Stop = true;
+                return;
+            }
+            e.Frames = signal.Length;
+            signal.CopyTo(e.Buffer);
+        }
+
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {
+            source = new AudioCaptureDevice()
+            {
+                DesiredFrameSize = 4096,
+                SampleRate = 22050,
+                Format = SampleFormat.Format16Bit
+            };
+
+            source.NewFrame += source_NewFrame;
+            source.AudioSourceError += source_AudioSourceError;
+            current = new float[source.DesiredFrameSize];
+
+            stream = new MemoryStream();
+            encoder = new WaveEncoder(stream);
+            source.Start();
+            updateButtons();
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+
+            decoder = new WaveDecoder(stream);
+
+            if (trackBar1.Value < decoder.Frames)
+                decoder.Seek(trackBar1.Value);
+            trackBar1.Maximum = decoder.Samples;
+
+            output = new AudioOutputDevice(this.Handle, decoder.SampleRate, decoder.Channels);
+
+            output.FramePlayingStarted += output__FramePlayingStarted;
+            output.NewFrameRequested += output_NewFrameRequested;
+            output.Stopped += output_PlayingFinished;
+
+            output.Play();
+
+            updateButtons();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (source != null)
+            {
+                source.SignalToStop();
+                source.WaitForStop();
+            }
+            if (output != null)
+            {
+                output.SignalToStop();
+                source.WaitForStop();
+            }
+
+            updateButtons();
+            Array.Clear(current, 0, current.Length);
+
+            updateWaveForm(current, current.Length);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (source != null) source.SignalToStop();
+            if (output != null) source.SignalToStop();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Stream fileStream = saveFileDialog1.OpenFile();
+            stream.WriteTo(fileStream);
+            fileStream.Close();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog(this);
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbLength.Text = String.Format("{0:00,00} sec.", duration.Seconds);
+        }
+    }
+}
